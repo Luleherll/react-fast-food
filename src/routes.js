4db335726f1238dfdcef90f459f7d81871e12a6c
Luleherll <luleherll@gmail.com>
2019-02-07 09:45:18 +0300

    chore(setup):setup-react-app
    - setup webpack
    - intergate express with webpack
    - setup linting using eslint
    - setup routing using react-router-dom
    - setup redux
    - install dependencies
    
    [Delivers #163781605]

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomeView from './views/mainView';
+import UserView from './views/userView';
+
+const store = configureStore();
+
+export default () => (
+    <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={HomeView} exact />
+            <Route path="/admin" component={UserView} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    )
