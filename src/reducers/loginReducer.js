89cb2af20dc219a592601ddbda67c2ed10ee451b
Luleherll <luleherll@gmail.com>
2019-02-08 14:47:23 +0300

    feat(food menu):Users should be able to view the menu
    - Create components UserDashboard, FoodList, FoodItem
    - Update userView component
    - Modify OrderItem component
    - Fix login logic
    - Implement getting of all food items
    
    [Delivers #163812086]

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,27 +1,26 @@
 import notify from 'msg-notify';
 import 'msg-notify/dist/notify.css';
-import * as types from '../actions/actionTypes'
+import * as types from '../actions/actionTypes';
 
 
 export default (state = {}, action) =>{
-    switch (action.type) {
+  switch (action.type) {
         
-        case types.LOGIN:
-            localStorage.setItem('user', action.data.token)
-            localStorage.setItem('Admin', action.data.Admin)
-            return state
+    case types.LOGIN:
+      localStorage.setItem('user', action.data.token);
+      localStorage.setItem('Admin', action.data.Admin);
+      return state;
 
-        case types.SIGNUP:
-            notify(action.data, 'success')
-            return state
+    case types.SIGNUP:
+      notify(action.data, 'success');
+      return state;
 
         
-        case types.ERROR_OCCURRED:
-            return {
-                ...state,
-                error: action.errMsg
-            }
-        default:
-            return state;
-    }
-}
+    case types.ERROR_OCCURRED:
+      localStorage.setItem('error', action.errMsg.error);
+      return state;
+
+    default:
+      return state;
+  }
+};
--- src/views/userView.js
+++ src/views/userView.js
@@ -4,26 +4,62 @@ import * as AllActions from '../actions/Thunks';
 import { bindActionCreators } from 'redux';
 import Header from '../components/common/Header';
 import AdminDashboard from '../components/admin';
+import UserDashboard from '../components/customer';
 
 
 class UserView extends React.Component{
     state = {
       nav : {
-        navItem1: this.showAllOrders,
-        navItem2: this.showPendingOrders,
-        navItem3: this.showArchivedOrders
+        navItem1: {name: 'menu', action: ()=>this.displayPage('foodItems', 'foodItems')},
+        navItem2: {name: 'myOrders'},
+        navItem3: {name: 'history'}
       },
-      adminProp: this.props.newOrders || [],      
+      adminProp: this.props.newOrders || [],
+      foodItems: this.props.foodItems || []
     }
-    displayPage = page => this.setState({ adminProp: this.props[page] || [] });
-    render(){
-      const {adminProp} = this.state;
-      const adminProps = {adminProp};
+
+    componentDidMount(){
       const Admin = localStorage.getItem('Admin');
+      if (Admin==='true') {
+        this.setState({
+          nav:{
+            navItem1: {name: 'Orders', action: ()=>this.displayPage('adminProp', 'newOrders')},
+            navItem2: {name: 'pending', action: ()=>this.displayPage('adminProp', 'allPendingOrders')},
+            navItem3: {name: 'archived', action: ()=>this.displayPage('adminProp', 'allArchivedOrders')}
+          }
+        });
+      }
+      
+    }
+
+    displayPage = (prop, page) => this.setState({ [prop]: this.props[page] || [] });
+    logOut = () =>{
+      localStorage.clear();
+      this.props.history.push('/');
+    }
+    getRoute = (pathname, adminProps, userProps) =>{
+      switch (pathname) {
+        case 'admin':
+          return(
+            
+            <AdminDashboard {...adminProps}/>
+          );
+
+        case 'user':
+          return(<UserDashboard foodItems={userProps}/>);
+      
+        default:
+          break;
+      }
+    }
+    render(){
+      const {adminProp, nav, foodItems} = this.state;
+      const adminProps = {adminProp};
+      const route = this.props.match.params.route;
       return(
         <div id='dash'>
-          <Header displayPage={this.displayPage}/>
-          { Admin ? <AdminDashboard {...adminProps}/> : ''}
+          <Header logout={this.logOut} nav={nav}/>
+          {this.getRoute(route, adminProps, foodItems)}
         </div>
       );
     }
